e803afe2823ceff16562b8f8566fdd31db816ffc
Niks Bugnēvics <70239899+flaffii@users.noreply.github.com>
2023-03-05 20:54:40 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -2,36 +2,41 @@
 
 import sys
 import threading
-import numpy
+import numpy as np
 
-def compute_height(n, parents):
-    tree = [[] for _ in range(n)]
 
-    for i in range(n):
-        if parents[i] == -1:
-            root = i
+def compute_max_height(num_nodes, parents_list):
+    children = {i: [] for i in range(num_nodes)}
+
+    root_nodes = []
+    for i, parent in enumerate(parents_list):
+        if parent == -1:
+            root_nodes.append(i)
         else:
-            tree[parents[i]].append(i)
+            children[parent].append(i)
 
-    def calculate_height(node):
-        heights = []
-        for child in tree[node]:
-            heights.append(calculate_height(child))
-        if not heights:
-            return 1
-        return 1 + max(heights)
+    def find_max_depth(node, depth):
+        if not children[node]:
+            return depth
+        else:
+            max_depth = 0
+            for child in children[node]:
+                child_depth = find_max_depth(child, depth+1)
+                max_depth = max(max_depth, child_depth)
+            return max_depth
 
-    max_height = calculate_height(root)
+    max_height = 0
+    for root in root_nodes:
+        height = find_max_depth(root, 0)
+        max_height = max(max_height, height)
+    return max_height + 1
 
-    return max_height
 
 def main():
-    n = int(input())
+    num_nodes = int(input())
+    parents_list = list(map(int, input().split()))
+    print(compute_max_height(num_nodes, parents_list))
 
-    parents_str = input()
-    parents = list(map(int, parents_str.split()))
-
-    print(compute_height(n, parents))
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
@@ -39,5 +44,5 @@ def main():
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
-main()
+
 
